470de03c0e5ec71b69bd5d48a44725f0cc2d82d6
Alexander Mellus <alexmellus5@gmail.com>
2016-06-10 20:48:56 -0400

    added ColorGrid

--- src/main/ColorGrid.java
+++ src/main/ColorGrid.java
@@ -1,7 +1,22 @@
 package main;
 
+import java.awt.*;
+
 /**
  * Created by flyer_000 on 9/15/2015.
  */
 public class ColorGrid {
+
+    private final int numCellsX;
+    private final int numCellsY;
+    private final Color[][] grid;
+
+    public ColorGrid(final int numCellsWide, final int numCellsHigh)
+    {
+        this.numCellsX = numCellsWide;
+        this.numCellsY = numCellsHigh;
+
+        this.grid = new Color[numCellsHigh][numCellsWide];
+
+    }
 }
